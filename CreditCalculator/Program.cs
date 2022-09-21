89d238682aaacf35b8911e20c902adb3c677950f
tbathiya <visioN1995>
2022-09-21 14:24:59 +1000

    Finalising the code/ removing comments

--- CreditCalculator/Program.cs
+++ CreditCalculator/Program.cs
@@ -9,15 +9,6 @@ namespace CreditCalculator
 			var newProcess = new ProcessCredit(new AnzCreditCalculator());
 			var customerToReturn = new Customer(800, 0, 10, 30);
 			Console.WriteLine($"3 Can get {newProcess.ProcessCustomerCredit(customerToReturn)}");
-
-			// Dependence Injection
-			// If we want to create a new calculator for USA market, we can smiply create a new class 
-			// called UsaCreditCalculator and uses ICreditCalculator interface. 
-			// then we can change the CalculateCredit() method according to the needs for USA market 
-			// and simply make a new process, 
-			// 	var newProcess = new ProcessCredit(new UsaCreditCalculator());
-			// This way, the actual ProcessCustomerCredit() method inside ProcessCredit is independent with the
-			// CreditCalculator class. 
 		}
 	}
 }
